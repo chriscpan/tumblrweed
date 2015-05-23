78238c860a5fac0d1c4f3fae0d6ca024c7c8503c
Chris <chrispy004@gmail.com>
2015-05-23 12:34:29 -0700

    input tumblr keys

--- app/controllers/tumblrweed_controller.rb
+++ app/controllers/tumblrweed_controller.rb
@@ -2,5 +2,24 @@ class TumblrweedController < ApplicationController
 
   def index
   end
-  
+
+  def show
+    @key = Tumblrweed::REST::Client.new Rails.application.secret.tumblr_keys,
+      Rails.application.secret.consumer_key
+    @secret = Tumblrweed::REST::Client.new Rails.application.secret.tumblr_keys,
+      Rails.application.secret.consumer_secret
+    @oauth_token = Tumblrweed::REST::Client.new Rails.application.secret.tumblr_keys,
+      Rails.application.secret.oauth_token
+    @oauth_token_secret = Tumblrweed::REST::Client.new Rails.application.secret.tumblr_keys,
+      Rails.application.secret.oauth_token_secret
+
+    @my_client = Tumblr::Client.new(
+      consumer_key: @key,
+      consumer_secret: @secret,
+      oauth_token: @oauth_token,
+      oauth_token_secret: @oauth_token_secret
+    )
+
+    # @my_client.posts(@input)
+  end
 end
